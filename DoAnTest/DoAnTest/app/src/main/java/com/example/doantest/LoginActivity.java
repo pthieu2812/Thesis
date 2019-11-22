94eba3a3307a056e338b849ecbab0376a7cd85e0
pthieu2812 <pthieu2812@gmail.com>
2019-11-22 07:28:08 +0700

    update Progress dialog

--- DoAnTest/DoAnTest/app/src/main/java/com/example/doantest/LoginActivity.java
+++ DoAnTest/DoAnTest/app/src/main/java/com/example/doantest/LoginActivity.java
@@ -3,8 +3,10 @@ package com.example.doantest;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -49,23 +51,43 @@ public class LoginActivity extends AppCompatActivity {
                     password.setError("Please enter password");
                     password.requestFocus();
                 }
-                else if (!email.isEmpty() && !pwd.isEmpty()) {
-                    mFirebaseAuth.signInWithEmailAndPassword(email,pwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(!task.isSuccessful()) {
-                                Toast.makeText(LoginActivity.this, "Tên tài khoản hoặc mật khẩu không đúng! Vui lòng thử lại", Toast.LENGTH_LONG).show();
-                            }
-                            else {
-                                Toast.makeText(LoginActivity.this, " Đăng nhập thành công", Toast.LENGTH_LONG).show();
-                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                startActivity(intent);
-                            }
-                        }
-                    });
+                else {
+                    progressDialog();
                 }
             }
         });
 
+    }
+
+    public void progressDialog(){
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,R.style.Theme_AppCompat_Light_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        new Handler().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        String email = emailID.getText().toString();
+                        String pwd = password.getText().toString();
+                        if (!email.isEmpty() && !pwd.isEmpty()) {
+
+                            mFirebaseAuth.signInWithEmailAndPassword(email,pwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(!task.isSuccessful()) {
+                                        Toast.makeText(LoginActivity.this, "Tên tài khoản hoặc mật khẩu không đúng! Vui lòng thử lại", Toast.LENGTH_LONG).show();
+                                    }
+                                    else {
+                                        Toast.makeText(LoginActivity.this, " Đăng nhập thành công", Toast.LENGTH_LONG).show();
+                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                    }
+                                }
+                            });
+                        }
+                    }
+                },1000);
     }
 }
